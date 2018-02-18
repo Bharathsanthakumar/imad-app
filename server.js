0f86b1beb40c5297c3c340a7d3cba91746604051
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-02-19 02:05:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,78 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var about={
+    title:Bharath | about,
+    heading:Bharath,
+    content:
+            `<div class="container">
+            <p> I am Bharath Santhakumar,</p>
+            <p>S/o Santhakumar</p>
+            <p>I am currently learning the basics of internet application and javascript</p>
+            <p>This is all about me :-/</p>
+            </div>`
+    
+};
+
+var education={
+    title:Bharath | education,
+    heading:'Bharath Santhakumar',
+    content:
+            `<div class="container">
+                <table>
+        	        <tbody>
+                	    <tr color="blue">
+                		<th>SI.no</th>
+                		<th>Institution</th>
+                		</tr>
+                		<tr>
+                		<td>1</td>
+                		<td>Ayira vaisya Secondary school</td>
+                		</tr>
+                		<tr>
+                		<td>2</td>
+                		<td>Don Bosco Matriculation School</td>
+                		</tr>
+                		<tr>
+                		<td>3</td>
+                		<td>Srv matric higher secondary school</td>
+                		</tr>
+                		<tr>
+                		<td>4</td>
+                		<td>Sri manaukula vinayagar enginnering college</td>
+                		</tr>
+        		    </tbody>
+                 </table>
+            </div>`
+};
+
+function templates(data)
+{
+    title=data.title;
+    heading-data.heading;
+    content-data.content;
+    var htmltemplate=`<!DOCTYPE html>
+                        <html>
+                            <head>
+                                $(title)
+                                <link href="/ui/style.css" rel="stylesheet" />
+                                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                            </head>
+                            <body>
+                                <h3>$(heading)</h3>
+                                <br/>
+                                $(content)
+                            </body>
+                        </html>`;
+    return (htmltemplate);                    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(templates(about));
 });
 
 app.get('/article-two', function (req, res) {
