3b2a93339ba151c7b31dd79163092dde7fbec57d
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-02-19 03:09:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/pic', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'pic.jpg'));
 });
 
