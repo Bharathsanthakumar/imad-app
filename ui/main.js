8a50146d5f4befccf93b55d74c107fbc85f34820
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-02-25 19:37:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,21 +21,37 @@ button.onClick = function()
     request.send(null);
     
 };
-/*
-var name=document.getElementById('name_input');
-var names=name.value;
+
+
 var submit=document.getElementById('sub');
 
 submit.onClick = function()
 {
-    var names = ['name1','name2','name3','name4','name5','name5'];
-    var htmllist='';
-    for(var i=0;i<names.length;i++)
-    {
-        htmllist="<ul>"+names[i]+"</ul>";
-    }
+    var request=new XMLHttpRequest();
     
-    var listt=document.getElementById('list');
-    listt.innerHTML=htmllist;
+    request.onreadystatechange =function()
+    {
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names = ['name1','name2','name3','name4','name5','name5'];
+                var htmllist='';
+                for(var i=0;i<names.length;i++)
+                {
+                    htmllist="<ul>"+names[i]+"</ul>";
+                }
+    
+                var listt=document.getElementById('list');
+                listt.innerHTML=htmllist;
+            }
+        }
+    };
+    
+    request.open('GET' , 'http://bharathsanthakumar99.imad.hasura-app.io/names' ,true);
+    request.send(null);
+    
+    var name=document.getElementById('name_input');
+    var names=name.value;
+    
 };
-*/
