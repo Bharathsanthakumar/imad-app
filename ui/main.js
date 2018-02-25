6aeef31af764ca3defdb94ca26af24fc9c87a02a
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-02-25 20:15:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ submit.onClick = function()
                 var htmllist='';
                 for(var i=0;i<names.length;i++)
                 {
-                    htmllist+="<ul>"+names[i]+"</ul>";
+                    htmllist+='<li>'+names[i]+'</li>';
                 }
     
                 var listt=document.getElementById('list');
