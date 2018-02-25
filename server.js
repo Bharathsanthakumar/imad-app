33a0e187ec30e1f7cfb9dc686cf3ffea81a8d432
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-02-25 19:52:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/counter',function (req,res){
 
 var names =[];
 
-app.get('/:names' , function(req,res) {
+app.get('/naming/:names' , function(req,res) {
    var named=req.query.names;
    names.push(named);
    res.send(JSON.stringify(names));
