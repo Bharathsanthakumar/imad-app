9ca5553b7028d28910c9e3223baec0e6c95d58d0
Bharath S <33080661+Bharathsanthakumar@users.noreply.github.com>
2018-03-07 20:22:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'bharathsanthakumar99',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password: process.env.DB_PASSWORD
+    password: 'db-bharathsanthakumar99-17838'
 };
 
 var app = express();
